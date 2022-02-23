12eeee8f3583b75548891e122afcd9acc45d5649
lukabele <ibeleluca@gmail.com>
2022-02-23 20:48:31 +0100

    posodobljeno

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,10 +1,14 @@
 import java.util.*;
+import java.util.Math;
 
 
 public class Gravitacija{
 	
 	public static double izracun(double h){
-		return 0;
+		double c = 6.674*Math.pow(10, -11);
+		double m = 5.972*Math.pow(10, 24);
+		double r = 6.371*Math.pow(10, 6);
+		return (c*m/(Math.pow((r+h), 2)));
 	}
 	
 	public static void main(String[] args){
@@ -14,6 +18,7 @@ public class Gravitacija{
 		double nadmorska_visina = sc.nextDouble();
 		double gravitacijski_pospesek = izracun(nadmorska_visina);
 		
+
 		System.out.println("Pospesek je: " + gravitacijski_pospesek);
 	}
 }
